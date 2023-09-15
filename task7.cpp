32c0cb207a0d3aa930e6dc7bf2c1fc794d1e0edd
Muhammad Mahad Toosy <144408153+MahAD32@users.noreply.github.com>
2023-09-15 15:54:08 +0500

    Add files via upload

---
+++ task7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ main(){
+         cout<<"                      .                    "<<endl;
+         cout<<"                      .                       "<<endl;
+         cout<<"         _ /          .          \ _                   "<<endl;
+         cout<<"        / /    /\    / \    /\    \ \                      "<<endl;
+         cout<<"       / /    /_/____| |____\_\    \ \               "<<endl;
+         cout<<"      /_/____/                 \____\_\                   "<<endl;
+         cout<<"     /       \\ |\         /| //       \          "<<endl;
+         cout<<"  __/__\      \\||         ||//      /__\__    "<<endl;
+         cout<<"  \  __ \      \_____________/      / __  /              "<<endl;
+         cout<<"         \___________|  |__________/                                       "<<endl;
+         cout<<"                    \____/                            "<<endl;
+         cout<<"                                             "<<endl;
+     }
