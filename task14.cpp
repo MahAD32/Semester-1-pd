6f84b13281ed5ae6617d6c4d0109595421f7569d
Muhammad Mahad Toosy <144408153+MahAD32@users.noreply.github.com>
2023-09-16 14:34:55 +0500

    Add files via upload

---
+++ task14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main(){
+     system("color 45");
+        cout<<------------------------"<<endl;
+        cout<<"    o      ^___^              "<<endl;
+        cout<<"      o    (o o)\___________          "<<endl;
+        cout<<"                                  "<<endl;
+        cout<<"           (___)\           )\/\                      "<<endl;
+        cout<<"                 ||-------w |                 "<<endl;
+        cout<<"                 ||        ||            "<<endl;
+       print 0;
+       }
---
+++ task17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main()   {
+     system("color 45");
+       cout<<"     +--^------------,----------,-----,----^-,      "<<endl;
+       cout<<"      |  |||||||      ,----------,     |      O     "<<endl;
+       cout<<"      +--------------------------------^----- |     "<<endl;
+       cout<<"        ',----------,----------,-------------'      "<<endl;
+       cout<<"         / xxxxxx /,|       /'                      "<<endl;
+       cout<<"        / xxxxxx /  |      /                        "<<endl;
+       cout<<"       / xxxxxx / '-------'                         "<<endl;
+       cout<<"      / xxxxxx /                                    "<<endl;
+       cout<<"     / xxxxxx /                                     "<<endl;
+       cout<<"    (--------(                                      "<<endl;
+       cout<<"     '------'                                       "<<endl;
+     return 0 ;
+      }
